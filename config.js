c8fe977a015940f22b101fb370a40e2dc883756d
sandeepjayasu <sandeepbandarajayasundara@gmail.com>
2025-12-29 21:04:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?JG267S'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?ko3Za9'
 };
